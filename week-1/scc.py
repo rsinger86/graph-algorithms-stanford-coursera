0724a6716b859c9238c500d8193c77d6978d7c0f
Robert Singer <robertgsinger@gmail.com>
2018-10-11 23:58:34 -0700

    Finishes week 1 problem to find SCCs of direct acyclic graph

--- week-1/scc.py
+++ week-1/scc.py
@@ -1,69 +1,98 @@
 from typing import List, Tuple
 from collections import defaultdict
-import random
-import copy
+import sys, resource, time
+
+# Increase recursion limit and stack size
+sys.setrecursionlimit(2 ** 20)
+hardlimit = resource.getrlimit(resource.RLIMIT_STACK)[1]
+resource.setrlimit(resource.RLIMIT_STACK,(hardlimit,hardlimit))
 
 
-def load_graph() -> Tuple[dict, int]:
+def load_data() -> Tuple[dict, dict, int]:
     graph = defaultdict(lambda: {'explored': False, 'connected': []})
-    max_node = None
+    graph_r = defaultdict(lambda: {'explored': False, 'connected': []})
+    max_node = -1
+    graph_file = 'data/graph.txt'
 
-    with open('data/graph.txt', 'r') as f:
+    with open(graph_file, 'r') as f:
         for line in f:
             from_node, to_node = line.strip().split(' ')
             from_node, to_node = int(from_node), int(to_node)
             graph[from_node]['connected'].append(to_node)
+            graph_r[to_node]['connected'].append(from_node)
 
-            if max_node is None or from_node > max_node:
+            if from_node > max_node:
                 max_node = from_node
 
-    return graph, max_node
+    return graph, graph_r, max_node
 
 
-def reverse_graph(graph):
-    graph_r = defaultdict(lambda: {'explored': False, 'connected': []})
+def collect_nodes_of_leader(graph: dict, leader: int) -> List[int]:
+    graph[leader]['explored'] = True
+    found = []
+    q = [leader]
 
-    for node in graph:
-        for out in graph[node]:
-            graph_r[out]['connected'].append(node)
+    while len(q) > 0:
+        current_node = q.pop()
+        found.append(current_node)
 
-    return graph_r
+        for n in graph[current_node]['connected']:
+            if graph[n]['explored'] is False:
+                graph[n]['explored'] = True
+                q.append(n)
+
+    return found
 
 
-def dfs_first_pass(graph: dict, node: int, finishes: List[int]):
+def second_pass_loop(graph: dict, ranked_nodes: List[int]) -> dict:
+    sccs = {}
+
+    while len(ranked_nodes) > 0:
+        leader = ranked_nodes.pop()
+
+        if not graph[leader]['explored']:
+            sccs[leader] = collect_nodes_of_leader(graph, leader)
+
+    return sccs
+
+
+def dfs_finish_times(graph: dict, node: int, ranked_finishes: List[int]):
     graph[node]['explored'] = True
 
-    for connected_node in graph[node]['connected']:
-        dfs(graph, connected_node, finishes)
+    for n in graph[node]['connected']:
+        if not graph[n]['explored']:
+            dfs_finish_times(graph, n, ranked_finishes)
 
-    finishes.append(node)
+    ranked_finishes.append(node)
 
 
-def dfs_loop_first_pass(graph, start_node: int) -> List[int]:
-    finishes: List[int] = []
+def first_pass_loop(graph, start_node: int) -> List[int]:
+    ranked_finishes: List[int] = []
     current_node = start_node
 
     while current_node > 0:
-        for connected_node in graph[current_node]['connected']:
-            if graph[connected_node]['explored']:
-                continue
-            
-            current_leader = current_node
-
-            dfs_first_pass(graph, 
-                connected_node, 
-                current_finish_rank,
-                finishes)
-
+        if not graph[current_node]['explored']:
+            dfs_finish_times(graph, current_node, ranked_finishes)
         current_node -= 1
-    
-    return finishes
+
+    return ranked_finishes
 
 
-graph, max_node = load_graph()
-print('Done building graph', max_node)
-graph_r = reverse_graph(graph)
-print('Done reversing')
-dfs_loop_first_pass(graph_r, max_node)
-print('Done with dfs loop')
+graph, graph_r, max_node = load_data()
+finish_ranks = first_pass_loop(graph_r, max_node)
+sccs = second_pass_loop(graph, finish_ranks)
 
+five_largest_sizes = []
+
+for leader in sccs:
+    size = len(sccs[leader])
+
+    if len(five_largest_sizes) < 5:
+        five_largest_sizes.append(size)
+        five_largest_sizes = sorted(five_largest_sizes, reverse=True)
+    elif size > five_largest_sizes[4]:
+        five_largest_sizes[4] = size
+        five_largest_sizes = sorted(five_largest_sizes, reverse=True)
+
+# [434821, 968, 459, 313, 211]
+print(five_largest_sizes)
